33d757db04a6b99bb69379c47392d710cd1e5bb5
cksrb63 <cksr1@naver.com>
2024-03-25 15:37:08 +0900

    Fix: build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const dependencies = { state: ["zustand"] };
 
 export default defineConfig({
   build: {
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            const modules = ['react', 'react-dom','react-router-dom'];
-            const chunk = modules.find(module => id.includes(`/node_modules/${module}`));
-            const packageName = id.match(/node_modules\/(.+?)\//);
-            if(chunk) return 'vendor';
-            if(packageName) return packageName[1];
-          }
-        }
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          ...dependencies,
+        },
       },
     },
   },
   plugins: [react()],
-})
+});
